e7b0960f4d341dc29327fcdd513337911b5487c6
Brit Butler <redline6561@gmail.com>
2016-11-15 11:44:37 -0500

    Get deletion working.

--- src/js/controllers/gif.js
+++ src/js/controllers/gif.js
@@ -16,6 +16,17 @@ function GifController ($scope, $http) {
     $http.post(SERVER_URL, gif).then(function (resp) {
       let gif = resp.data;
       $scope.gifs.push(gif);
+      console.log($scope.gifs);
+    });
+  };
+
+  $scope.deleteMe = function (gif) {
+    $http.delete(SERVER_URL + gif._id).then(function (resp) {
+      console.log(resp);
+      $scope.gifs = $scope.gifs.filter(function (x) {
+        return x._id !== gif._id;
+      });
+//      $scope.gifs = $scope.gifs.filter(x => x._id !== gif._id);
     });
   };
 
